e636c57e2fa84f904302a6b98101a771df1122da
ketr4x <ketraxpl@gmail.com>
2026-03-12 21:06:49 +0100

    feat: initialized Firebase and FlutterFire configuration

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -9,6 +12,7 @@ android {
     namespace = "com.ketr4x.pingpal"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+    android.buildFeatures.buildConfig = true
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -22,12 +26,14 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.ketr4x.pingpal"
+        namespace = "com.ketr4x.pingpal"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        android.buildFeatures.buildConfig = true
     }
 
     buildTypes {
