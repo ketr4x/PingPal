41b3d65acbe456962af3c4fa67e8efd2e046e661
ketr4x <mail@ketrax.ovh>
2026-06-01 16:58:49 +0200

    feat: add user deletion handler to remove friends from deleted user

--- functions/main.py
+++ functions/main.py
@@ -36,3 +36,26 @@ def on_ping(event: firestore_fn.Event[firestore_fn.DocumentSnapshot]) -> None:
         })
 
     print(f"Notification send for {receiver}")
+
+@firestore_fn.on_document_deleted(document="Users/{userId}")
+def on_user_deleted(event: firestore_fn.Event[firestore_fn.DocumentSnapshot]) -> None:
+    deleted_uid = event.params["userId"]
+    db = firestore.client()
+
+    batch = db.batch()
+    batch_count = 0
+
+    users_with_friend = db.collection("Users").where("friends", "array_contains", deleted_uid).get()
+    for user_doc in users_with_friend:
+        user_ref = db.collection("Users").document(user_doc.id)
+        batch.update(user_ref, {
+            "friends": firestore.ArrayRemove([deleted_uid])
+        })
+        batch_count += 1
+
+        if batch_count >= 450:
+            batch.commit()
+            batch = db.batch()
+            batch_count = 0
+
+    print(f"Friends deleted for {deleted_uid}")
